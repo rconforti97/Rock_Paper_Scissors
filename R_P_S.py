a8de973fa00f42db30210558e699435cf9eb140b
Rachel Conforti <50918318+rconforti97@users.noreply.github.com>
2020-10-10 17:56:37 -0400

    Add files via upload
    Fixed some of the code. Got rid of redundant print lines. Fixed some spelling errors. Took out unnecessary comments.

--- R_P_S.py
+++ R_P_S.py
@@ -1,20 +1,16 @@
-# A bunch of if statements
+# random is here for the random choice we have the computer making.
 import random
 
 option = ['Rock', 'Paper', 'Scissors']
 quit_loop = False
 user_score = 0
 computer_score = 0
-# computer_score = 0
-# user_score
-# user_input = input("Your Choice: ")
-# print(user_input)
-# Rock > Scissors
-# Paper > Rock
-# Scissors > Paper
+
 
 def comparator(user_input):
+    # We want it to re-choose the random choice each time so it goes into the function
     random_option = random.choice(option)
+    # We have to declare them 'global' here because otherwise it does not recognize
     global computer_score
     global user_score
     if (user_input == 'Rock' and random_option == 'Rock' or user_input == 'Paper' and random_option == 'Paper'
@@ -38,23 +34,24 @@ def comparator(user_input):
 def round_checker(user_score, computer_score):
     global quit_loop
     if user_score == 5:
-        print("------------------------------------------------------")
-        print("Heck yeah! You just showed that computer who's boss! Congratulations on winning.")
+        print("------------------------------------------------------\nHeck yeah! You just showed that "
+              "computer who's boss! Congratulations on winning.")
         quit_loop = True
         return print("Thank you for playing!")
     elif computer_score == 5:
         quit_loop = True
-        print("------------------------------------------------------")
-        print("Oh no! The computer has won. :(")
+        print("------------------------------------------------------\nOh no! The computer has won. :(")
         return print("Thank you for playing!")
     else:
         return
 
-print("Welcome to Rock, Paper, Scissors! \n\nRules:\nWhen prompted pick 'Rock', 'Paper', or 'Scissors!'. Who ever wins 5 points first wins!.")
+
+print("Welcome to Rock, Paper, Scissors! made by Rachel Conforti.\nHere are the rules:\n1) When prompted pick "
+      "Rock, Paper, or Scissors. \n2) Who ever wins 5 points first wins! \n3) Your responses case sensitive."
+      "\n4) Have fun :)")
 
 while quit_loop is not True:
-    print("------------------------------------------------------")
-    print("What is you choice? Rock, Paper, or Scisscors? (it is case sensitive)")
+    print("------------------------------------------------------\nWhat is you choice? Rock, Paper, or Scissors?")
     user_input = input()
     comparator(user_input)
     round_checker(user_score, computer_score)
