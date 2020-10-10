961dbfa3417d24264d9e18ebc8eb1c3e14137916
Rachel Conforti <50918318+rconforti97@users.noreply.github.com>
2020-10-10 18:23:42 -0400

    Add files via upload
    Made it so you don't have to capitalized your answer.

--- R_P_S.py
+++ R_P_S.py
@@ -9,21 +9,21 @@ computer_score = 0
 
 def comparator(user_input):
     # We want it to re-choose the random choice each time so it goes into the function
-    random_option = random.choice(option)
+    random_option = random.choice(option).lower()
     # We have to declare them 'global' here because otherwise it does not recognize
     global computer_score
     global user_score
-    if (user_input == 'Rock' and random_option == 'Rock' or user_input == 'Paper' and random_option == 'Paper'
-            or user_input == 'Scissors' and random_option == 'Scissors'):
+    if (user_input == 'rock' and random_option == 'rock' or user_input == 'paper' and random_option == 'paper'
+            or user_input == 'scissors' and random_option == 'scissors'):
         return print("The computer has also chosen: " + random_option + "\nSo it's a Tie - No one gets any points")
-    elif (user_input == 'Rock' and random_option == 'Scissors' or user_input == 'Paper' and random_option == 'Rock'
-          or user_input == 'Scissors' and random_option == 'Paper'):
+    elif (user_input == 'rock' and random_option == 'scissors' or user_input == 'paper' and random_option == 'rock'
+          or user_input == 'scissors' and random_option == 'paper'):
         print("The computer has chosen: " + random_option + "\nYou have won this round against the computer! "
                                                             "Point for you!")
         user_score += 1
         return print("\nYour score is: " + str(user_score) + "\nComputer score is: " +  str(computer_score))
-    elif random_option == 'Rock' and user_input == 'Scissors' or random_option == 'Paper' and user_input == 'Rock' \
-            or random_option == 'Scissors' and user_input == 'Paper':
+    elif random_option == 'rock' and user_input == 'scissors' or random_option == 'paper' and user_input == 'rock' \
+            or random_option == 'scissors' and user_input == 'paper':
         print("The computer has chosen: " + random_option + "\nThe computer has won! Point for the computer")
         computer_score += 1
         return print("\nYour score is: " + str(user_score) + "\nComputer score is: " + str(computer_score))
@@ -47,12 +47,11 @@ def round_checker(user_score, computer_score):
 
 
 print("Welcome to Rock, Paper, Scissors! made by Rachel Conforti.\nHere are the rules:\n1) When prompted pick "
-      "Rock, Paper, or Scissors. \n2) Who ever wins 5 points first wins! \n3) Your responses case sensitive."
-      "\n4) Have fun :)")
+      "Rock, Paper, or Scissors. \n2) Who ever wins 5 points first wins! \n3) Have fun :)")
 
 while quit_loop is not True:
     print("------------------------------------------------------\nWhat is you choice? Rock, Paper, or Scissors?")
-    user_input = input()
+    user_input = input().lower()
     comparator(user_input)
     round_checker(user_score, computer_score)
 
